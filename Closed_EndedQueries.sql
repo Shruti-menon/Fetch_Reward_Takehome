62a503761d88f0889dc67d5f35ac2f111db03bdf
Shruti-menon <31414566+Shruti-menon@users.noreply.github.com>
2025-01-28 16:34:26 -0800

    Update Closed_EndedQueries.sql

--- Closed_EndedQueries.sql
+++ Closed_EndedQueries.sql
@@ -2,34 +2,42 @@
 
 -- What are the top 5 brands by receipts scanned among users 21 and over?
 WITH Finaltable AS (
-    SELECT t.*, p.*,u.*
-	FROM TRANSACTION_TAKEHOME as t
-	INNER JOIN PRODUCTS_TAKEHOME as p
-	ON t.BARCODE = p.BARCODE
-	LEFT JOIN USER_TAKEHOME as u
-	ON t.USER_ID = u.ID
-	WHERE  t.FINAL_SALE is not null and t.FINAL_QUANTITY <> "zero" and t.BARCODE is not null 
+SELECT t.*, u.*, p.*
+FROM TRANSACTION_TAKEHOME as t
+INNER JOIN PRODUCTS_TAKEHOME as p
+ON t.BARCODE = p.BARCODE
+LEFT JOIN USER_TAKEHOME as u
+ON t.USER_ID = u.ID
+WHERE t.FINAL_SALE is not null 
+AND t.FINAL_QUANTITY <> "zero"
+AND t.BARCODE is not null
 )
-SELECT BRAND, CATEGORY_1, count(RECEIPT_ID) as cnt
-FROM Finaltable 
-WHERE (date() - date(BIRTH_DATE) )>=21
-GROUP by BRAND, CATEGORY_1
-ORDER By cnt DESC
-LIMIT 5
+SELECT 
+coalesce(BRAND, MANUFACTURER, CATEGORY_2) AS brand_name,
+count(receipt_id) AS cnt
+FROM Finaltable
+WHERE (date()-date(BIRTH_DATE))>=21
+GROUP BY 1
+ORDER BY cnt DESC
 
 -- What are the top 5 brands by sales among users that have had their account for at least six months?
 WITH Finaltable AS (
-    SELECT t.*, p.*,u.*
-	FROM TRANSACTION_TAKEHOME as t
-	INNER JOIN PRODUCTS_TAKEHOME as p
-	ON t.BARCODE = p.BARCODE
-	LEFT JOIN USER_TAKEHOME as u
-	ON t.USER_ID = u.ID
-	WHERE  t.FINAL_SALE is not null and t.FINAL_QUANTITY <> "zero" and t.BARCODE is not null 
+SELECT t.*, u.*, p.*
+FROM TRANSACTION_TAKEHOME as t
+INNER JOIN PRODUCTS_TAKEHOME as p
+ON t.BARCODE = p.BARCODE
+LEFT JOIN USER_TAKEHOME as u
+ON t.USER_ID = u.ID
+WHERE t.FINAL_SALE is not null 
+AND t.FINAL_QUANTITY <> "zero"
+AND t.BARCODE is not null
 )
-SELECT BRAND, MANUFACTURER, CATEGORY_1,RECEIPT_ID, BARCODE, sum(FINAL_SALE) as quantity
-FROM Finaltable 
-WHERE datetime(CREATED_DATE) <= datetime('now', '-6 months')
-GROUP by BRAND, MANUFACTURER, CATEGORY_1,RECEIPT_ID, BARCODE
-ORDER By quantity DESC
-LIMIT 5
+SELECT 
+coalesce(BRAND, MANUFACTURER, CATEGORY_2) AS brand_name,
+sum(FINAL_SALE) as cnt
+FROM Finaltable
+where CREATED_DATE <=  date('now', '-6 months')
+GROUP BY 1
+ORDER BY cnt DESC
+
+
